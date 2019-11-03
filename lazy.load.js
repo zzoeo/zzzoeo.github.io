c5fb77788d3dacd567d54c876a6f54d6d50c9720
zzoeo <zoeo.esplin@gmail.com>
2019-11-02 23:57:37 -0400

    w07-04

--- lazy.load.js
+++ lazy.load.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll("[data-src]")
+const images = document.querySelectorAll("img")
 
 function preloadImage(img) {
     const src = img.getAttribute("data-src");
